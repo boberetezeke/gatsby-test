c9025fa2fa93a6cdb3244261c454e5d549849d5f
stevetuckner <stevetuckner@gmail.com>
2020-01-06 22:18:44 -0600

    got parallax working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,44 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/global.css'
+import $ from 'jquery'
+
+function handleScroll() {
+    let menu = document.getElementById("menu");
+    let scrollTop = (document.documentElement.scrollTop || document.body.scrollTop)
+    let windowHeight = window.innerHeight;
+
+    var $el = $('#menu'); 
+    var isPositionFixed = ($el.css('position') == 'fixed');
+    if (scrollTop > windowHeight && !isPositionFixed){ 
+      $el.css({'position': 'fixed', 'top': '0px', 'width': '100%'}); 
+     console.log("setting fixed")
+    }
+    if ($(this).scrollTop() < windowHeight && isPositionFixed){
+      $el.css({'position': 'static', 'top': '0px'}); 
+      console.log("setting static")
+    } 
+}
 
 export default () => { 
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return function cleanup() {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    })
+
     return (
         <main>
-            <div className="parallax bg1"></div>
-            <section>
-                <h1>Congrats Seniors!</h1>
-            </section>
-            <div className="parallax bg2"> </div>
+            <div className="bg1 parallax"></div>
+            <section id="menu"> <h1>Congrats Seniors!</h1> </section>
+            <div className="bg2 parallax"> </div>
             <section> <h1>Emily</h1> </section> 
+            <div className="bg3 parallax"> </div>
+            <section> <h1>Hannah</h1> </section> 
+            <div className="bg4 parallax"> </div>
+            <section> <h1>Jessica</h1> </section> 
         </main>
      )
 }

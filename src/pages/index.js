4338730b3d904d8dc38e4dbfebd5c3fcda17302d
stevetuckner <stevetuckner@gmail.com>
2020-01-06 20:32:44 -0600

    got parallax to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/global.css'
 
-export default () => <div>Hello world!</div>
+export default () => { 
+    return (
+        <main>
+            <div className="parallax bg1"></div>
+            <section>
+                <h1>Congrats Seniors!</h1>
+            </section>
+            <div className="parallax bg2"> </div>
+            <section> <h1>Emily</h1> </section> 
+        </main>
+     )
+}

0ed4c34bb5c05004b41cee3637c2877285c6f92a
stevetuckner <stevetuckner@gmail.com>
2020-02-14 09:19:31 -0600

    forgot home page logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import BlogSection from "../components/blog_section"
 import ContactSection from "../components/contact_section"
 import FooterSection from "../components/footer_section"
 import  { handleScrollEffect } from "../lib/handle_scroll"
+import { Link } from 'gatsby'
 
 export default ({data}) => {
   const { edges: posts } = data.allMarkdownRemark;
@@ -16,9 +17,9 @@ export default ({data}) => {
     <main>
       <div className="bg1 parallax"></div>
       <section id="logo">
-        <a href="http://sortilege.online/" className="logo">
+        <Link to="/" className="logo">
           <img src="/cropped-Sortilege-Logo-2.png" />
-        </a>
+        </Link>
       </section>
       <section id="menu">
         <Menu/>

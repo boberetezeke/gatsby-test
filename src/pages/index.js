5dc1d7cc261e6f4907cfb8941d132a33cb8ecddb
stevetuckner <stevetuckner@gmail.com>
2020-02-14 08:36:40 -0600

    fix import for useEffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Menu from "./menu"
 import HomeSection from "./sections/home_section"
 import AboutSection from "./sections/about_section"
